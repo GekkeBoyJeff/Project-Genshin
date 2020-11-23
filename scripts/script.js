32994a01a4b2793ad3d7327a5d72a53296820fbc
GekkeBoyJeff <jeffrey.ullers@hva.nl>
2020-11-23 16:40:27 +0100

    Grote update html, css en js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+var navbar = document.querySelector(".navbar");
+var hamburger = document.querySelector(".header-toggle");
+
+hamburger.addEventListener("click", toggleHamburger);
+
+function toggleHamburger(){
+  	navbar.classList.toggle("showNav");
+  	hamburger.ClassList.toggle("showClose");
+}
