7b23560abdcc44e82412be54c6dd8dff3a600219
GekkeBoyJeff <jeffrey.ullers@hva.nl>
2020-11-24 13:54:02 +0100

    toggle naar add veranderd

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,22 @@
 // JavaScript Document
 
 // Variabelen navbar
-var navbar = document.querySelector("nav")
-var hamburger = document.querySelector(".header-toggle")
-var close = document.querySelector(".showClose")
+var navbar = document.querySelector("nav");
+var hamburger = document.querySelector(".header-toggle");
+var close = document.querySelector(".showClose");
 // Eind variabelen navbar
 
 // Variabelen zwart achtergrond
-var blackBackground = document.querySelector(".blackBackground")
-var blackBackgroundOff = document.querySelector(".blackBackgroundOn")
+var blackBackground = document.querySelector(".blackBackground");
+var blackBackgroundOff = document.querySelector(".blackBackgroundOn");
 // Eind variabelen zwart achtergrond
 
-hamburger.addEventListener("click", toggleHamburgerOn)
-close.addEventListener("click", toggleHamburgerOff)
+hamburger.addEventListener("click", toggleHamburgerOn);
+close.addEventListener("click", toggleHamburgerOff);
 
 function toggleHamburgerOn(){
-	navbar.classList.toggle("showNav");
-	blackBackground.classList.toggle("blackbackgroundOn");
+	navbar.classList.add("showNav");
+	blackBackground.classList.add("blackbackgroundOn");
 }
 
 function toggleHamburgerOff(){
