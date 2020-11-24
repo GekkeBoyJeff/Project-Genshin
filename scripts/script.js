7f7da3f500522883ca012ac4cb0328640e3d9559
GekkeBoyJeff <jeffrey.ullers@hva.nl>
2020-11-24 10:59:29 +0100

    Grote fix nav menu + black background

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,27 @@
 // JavaScript Document
 
-var navbar = document.querySelector(".navbar");
-var hamburger = document.querySelector(".header-toggle");
+// Variabelen navbar
+var navbar = document.querySelector("nav")
+var hamburger = document.querySelector(".header-toggle")
+var close = document.querySelector(".showClose")
+// Eind variabelen navbar
 
-hamburger.addEventListener("click", toggleHamburger);
+// Variabelen zwart achtergrond
+var blackBackground = document.querySelector(".blackBackground")
+var blackBackgroundOff = document.querySelector(".blackBackgroundOn")
+// Eind variabelen zwart achtergrond
 
-function toggleHamburger(){
-  	navbar.classList.toggle("showNav");
-  	hamburger.ClassList.toggle("showClose");
+hamburger.addEventListener("click", toggleHamburgerOn)
+close.addEventListener("click", toggleHamburgerOff)
+
+function toggleHamburgerOn(){
+	navbar.classList.toggle("showNav");
+	blackBackground.classList.toggle("blackbackgroundOn");
 }
+
+function toggleHamburgerOff(){
+	navbar.classList.remove("showNav");
+	blackBackground.classList.remove("blackbackgroundOn");
+}
+
+// BRON : https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0
